ee3505610d69947f7eec832be55aa0cd344c1dd2
Hector <h_carvajal@outlook.es>
2018-10-24 17:29:24 -0500

    Categories sites
    Sites by categories -- development with Angular 6 and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+
+// @angular Animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// @angular Material
+import { MaterialModule } from './angular-material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/categories/categories.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit {
+
+  title = 'Buscar Lugares por Categorías';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
